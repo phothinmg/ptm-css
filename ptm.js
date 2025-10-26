21927c892d524a5dc57bdea0515f61a594d15de7
phothinmg <phothinmg@disroot.org>
2025-10-27 03:00:56 +0630

    new file:   fonts/fonts.css

--- ptm.js
+++ ptm.js
@@ -1,27 +1,28 @@
-var doc = document.documentElement;
-var body = document.body;
-var head = document.head;
-// ===========================================================
 (function () {
-  var tablerLink = document.createElement("link");
-  tablerLink.rel = "stylesheet";
-  tablerLink.href =
-    "https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/dist/tabler-icons.min.css";
-  // ==
-  let topBtn = document.createElement("button");
-  topBtn.classList.add("top-btn");
-  topBtn.setAttribute("title", "Scroll To Top");
-  let icon = document.createElement("i");
-  icon.classList.add("ti");
-  icon.classList.add("ti-square-rounded-arrow-up");
-  topBtn.appendChild(icon);
-  body.appendChild(topBtn);
-  // On Click, Scroll to the page's top, replace 'smooth' with 'instant' if you don't want smooth scrolling
-  topBtn.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
+  var doc = document.documentElement;
+  var body = document.body;
+  var head = document.head;
+  var navBar = document.getElementById("nav");
+  var topBtn = document.createElement("button");
 
-  // On scroll, Show/Hide the btn with animation
-  window.onscroll = () => (topBtn.style.opacity = window.scrollY > 500 ? 1 : 0);
-  // ==
-
-  head.appendChild(tablerLink);
+  var domscripts = {
+    init: function init() {
+      var self = this;
+      self.scrollToTop();
+      self.onScroll();
+    },
+    scrollToTop: function scrollToTop() {
+      topBtn.classList.add("topBtn");
+      topBtn.setAttribute("title", "Scroll To Top");
+      body.appendChild(topBtn);
+      topBtn.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
+    },
+    onScroll: function onScroll() {
+      window.onscroll = () => {
+        topBtn.style.opacity = window.scrollY > 500 ? 1 : 0;
+        navBar.style.position = window.scrollY > 300 ? "fixed" : "relative";
+      };
+    },
+  };
+  domscripts.init();
 })();
